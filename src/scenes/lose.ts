63d3ced43dbdfc806b3ac791ef461c4a30401146
Mark <mark@remarkablemark.org>
2025-01-28 19:53:47 -0500

    feat(scenes): add button to go to title in lose

--- src/scenes/lose.ts
+++ src/scenes/lose.ts
@@ -3,27 +3,40 @@ import { addButton, addText } from '../gameobjects'
 
 scene(Scene.Lose, () => {
   const { x, y } = center()
-  const margin = 100
+  const buttonHeight = 80
 
   addText({
     width: 400,
     height: 100,
     x,
-    y: y - margin,
+    y: y - buttonHeight * 2,
     text: 'Game Over',
     fontSize: 48,
   })
 
   addButton({
     width: 220,
-    height: 80,
+    height: buttonHeight,
     radius: 8,
     x,
-    y: y + margin,
+    y: y + buttonHeight,
     text: 'Restart',
     onClick() {
       play(Sound.Hit)
       go(Scene.Game)
+    },
+  })
+
+  addButton({
+    width: 220,
+    height: buttonHeight,
+    radius: 8,
+    x,
+    y: y + buttonHeight * 2 + 20,
+    text: 'Exit',
+    onClick() {
+      play(Sound.Hit)
+      go(Scene.Title)
     },
   })
 })
